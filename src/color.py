3d6ecc195c904877b08764a0311d1d6973bdc655
memecoder <memecoder17@gmail.com>
2025-02-20 11:31:41 +0700

    Update color.py

--- src/color.py
+++ src/color.py
@@ -11,7 +11,7 @@ class Color(Vector):
             raise ValueError("Hex color must be 7 characters long")
         elif not all([c in "0123456789ABCDEF" for c in hexcolor[1:]]):
             raise ValueError("Hex color must be in the format #RRGGBB")
-        x = int(hexcolor[1:3], 16) / 255.0
-        y = int(hexcolor[3:5], 16) / 255.0
-        z = int(hexcolor[5:7], 16) / 255.0
-        return cls(x, y, z)
+        r = int(hexcolor[1:3], 16) / 255.0
+        g = int(hexcolor[3:5], 16) / 255.0
+        b = int(hexcolor[5:7], 16) / 255.0
+        return cls(r, g, b)
